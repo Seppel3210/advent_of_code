6f6fd0af7228365fbb123c4b7500444c4db65a0b
Sebastian Widua <seppel3210@gmail.com>
2023-11-30 22:36:10 +0100

    implement runner using build script

---
+++ src/day1.rs
@@ -0,0 +1,11 @@
+use std::fs;
+
+pub fn part1() {
+    let input = fs::read_to_string("input/day1.txt").unwrap();
+    println!("{input}");
+}
+
+pub fn part2() {
+    let input = fs::read_to_string("input/day1.txt").unwrap();
+    println!("{input}");
+}
