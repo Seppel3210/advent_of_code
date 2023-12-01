13969bea98d21d4baa94f1ec5f1f305a1e11be15
Sebastian Widua <seppel3210@gmail.com>
2023-12-01 16:38:37 +0100

    Solve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,10 +2,70 @@ use std::fs;
 
 pub fn part1() {
     let input = fs::read_to_string("input/day1.txt").unwrap();
-    println!("{input}");
+    let sum: u32 = input
+        .lines()
+        .map(|l| {
+            let first = l.chars().find(|c| c.is_digit(10)).unwrap();
+            let last = l.chars().rfind(|c| c.is_digit(10)).unwrap();
+            format!("{first}{last}").parse::<u32>().unwrap()
+        })
+        .sum();
+    println!("{sum}");
+}
+
+fn parse_line(line: &str) -> (u32, u32) {
+    let digits = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+    let mut first = 0;
+    'outer: for i in 0..line.len() {
+        if let Some(val) = line[i..].chars().nth(0).unwrap().to_digit(10) {
+            first = val;
+            break 'outer;
+        } else {
+            for &(name, val) in &digits {
+                if line[i..].starts_with(name) {
+                    first = val;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    let mut last = 0;
+    'outer: for i in (0..=line.len()).rev() {
+        if let Some(val) = line[..i].chars().last().unwrap().to_digit(10) {
+            last = val;
+            break 'outer;
+        } else {
+            for &(name, val) in &digits {
+                if line[..i].ends_with(name) {
+                    last = val;
+                    break 'outer;
+                }
+            }
+        }
+    }
+    (first, last)
 }
 
 pub fn part2() {
     let input = fs::read_to_string("input/day1.txt").unwrap();
-    println!("{input}");
+    let sum: u32 = input
+        .lines()
+        .map(|l| {
+            let (first, last) = parse_line(l);
+            println!("{l} => {first}, {last}");
+            format!("{}{}", first, last).parse::<u32>().unwrap()
+        })
+        .sum();
+    println!("{sum}");
 }
